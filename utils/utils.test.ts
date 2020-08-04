f04bbe96b87ef43a94ff7f15df682c8e2c8ab341
Vincent Iroleh <irolehiroleh@gmail.com>
2020-08-04 15:20:15 +0100

    using expect to handel errors

--- utils/utils.test.ts
+++ utils/utils.test.ts
@@ -1,11 +1,39 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     const result = utils.add(33, 11);
-    if (result !== 44) throw new Error(`Expected 44, but got ${result}.`)
+    expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     const result = utils.square(5);
-    if (result !== 25) throw new Error(`Expected 25, but got ${result}.`)
+    expect(result).toBe(25).toBeA('number');
 });
+
+it('should expect some values', () => {
+    expect(12).toNotBe(11);
+    expect({ name: 'Vincent' }).toEqual({ name: 'Vincent' })
+    expect({ name: 'Iroleh' }).toNotEqual({ name: 'Vincent' })
+    expect([2, 3, 4, 5]).toInclude(5);
+    expect([2, 3, 4, 5]).toExclude(1);
+    expect({
+        name: 'Vincent',
+        age: 25,
+        location: 'Nigeria'
+    }).toInclude({
+        age: 25
+    })
+});
+
+it('should set firstName and lastName', () => {
+    const user = {
+        age: 25,
+        location: 'Nigeria'
+    };
+
+    const result = utils.setName(user, 'Vincent Iroleh');
+    expect(result).toInclude({ firstName: 'Vincent', lastName: 'Iroleh' }).toBeA('object');
+
+})
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,10 @@
 module.exports.add = (a: number, b: number) => a + b;
 
 module.exports.square = (x: number) => x * x;
+
+module.exports.setName = (user: any, fullName: string) => {
+    let names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+};
